d5912b675956a8b84164ccc2da6d150b745e2d60
Amit Fenn <fenn@wzw.tum.de>
2021-12-18 00:32:36 +0100

    better representation of where I am.
    Former-commit-id: 662df3d4b3136d80a83b5d94ca246f626c2d78d4

--- clientside/config.sh
+++ clientside/config.sh
@@ -6,6 +6,7 @@
 #This file hosts the variables for DASiRe's preprocessing.
 fasta=/MOUNT/input/GRCh38.primary_assembly.genome.fa
 gtf=/MOUNT/input/gencode.v36.annotation.gtf
+gff=/MOUNT/input/gencode.v36.annotation.gff
 transcripts_fasta=/MOUNT/input/gencode.v36.transcripts.fa
 exon_custom_annotation=/MOUNT/input/gencode.v36.annotation.dexseq.gff
 metadata=/MOUNT/input/metadata.txt
